adbad231e038cacf639960462a91c39064c56eb4
Tobias Skjelvik <trampcha@gmail.com>
2019-12-30 15:23:58 +0800

    code cleanup and some unpushed algorithms

--- code/NaiveBayes/sklearn_naivebayes.py
+++ code/NaiveBayes/sklearn_naivebayes.py
@@ -1,16 +1,18 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import naive_bayes
+import time
 
-trainData = pd.read_csv("../../input/train.csv").values
-testData = pd.read_csv("../../input/test.csv").values
+trainData = pd.read_csv("./input/train.csv").values
+testData = pd.read_csv("./input/test.csv").values
 
-clf = naive_bayes.BernoulliNB()
+# clf = naive_bayes.BernoulliNB()
+clf = naive_bayes.GaussianNB()
 
 #Training dataset
 xTrain = trainData[0:, 1:]
 yTrain = trainData[0:,0]
-
+t1 = time.time()
 clf.fit( X=xTrain, y=yTrain)
 
 # depth of the decision tree
@@ -18,9 +20,11 @@ clf.fit( X=xTrain, y=yTrain)
 
 # Predict value
 predtictY = clf.predict(testData)
-
+t2 = time.time()
+print("training time:", str(t2-t1))
 # Create submission file
 df_sub = pd.DataFrame(list(range(1,len(testData)+1)))
 df_sub.columns = ["ImageID"]
 df_sub["Label"] = predtictY
-df_sub.to_csv("../../output/sklearn_BernoulliNaiveBayes.csv",index=False)
+# df_sub.to_csv("./output/sklearn_BernoulliNaiveBayes.csv",index=False)
+df_sub.to_csv("./output/sklearn_GaussianNaiveBayes.csv",index=False)
--- code/random-forest/sklearn_rand_forest.py
+++ code/random-forest/sklearn_rand_forest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import time
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 
 
@@ -11,19 +11,15 @@ print(trainData.shape)
 #Training dataset
 xTrain = trainData[0:, 1:]
 yTrain = trainData[0:,0]
-#model = LogisticRegression()
-model = LogisticRegression(
-    penalty='l1', solver='saga', tol=0.1
-)
+model = RandomForestClassifier(n_estimators=100)
 t1 = time.time()
 model.fit(xTrain, yTrain)
-
 # Predict value
 predtictY = model.predict(testData)
 t2 = time.time()
-print("Time training:", str(t2-t1))
+print("training time:", str(t2-t1))
 # Create submission file
 df_sub = pd.DataFrame(list(range(1,len(testData)+1)))
 df_sub.columns = ["ImageID"]
 df_sub["Label"] = predtictY
-df_sub.to_csv("./output/new_sklearn_LR.csv",index=False)
+df_sub.to_csv("./output/randomForestn100sk.csv",index=False)
